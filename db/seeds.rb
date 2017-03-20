c20b1196c9829e6020d869e83ee02ac31bf80ef6
Aizaz Khaja <aizazkj@gmail.com>
2017-03-20 16:19:31 -0400

    fixed assign17 issue where unique comment wasn't being seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@ require 'random_data'
   )
 end
 
-Post.find_or_create_by!(
+# Create a unique post that doesn't repeat when seeded.
+post_151 = Post.find_or_create_by!(
   title: "blablablabla",
   body: "more bla"
 )
@@ -23,10 +24,11 @@ posts = Post.all
   )
 end
 
-# Comment.find_or_create_by!(
-#   post: 151,
-#   body: "more bla"
-# )
+# Create a unique comment in the above referenced unique post.
+Comment.find_or_create_by!(
+  post: post_151,
+  body: "that is a lot of bla"
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
