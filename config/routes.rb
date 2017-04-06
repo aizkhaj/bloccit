7c9436d01222f02266fc2121bd9fe67f0e174269
Aizaz Khaja <aizazkj@gmail.com>
2017-04-06 11:11:58 -0400

    topics added, nested posts under topics and changed related stuffs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :topics
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+  # We pass resources :posts to the resources :topics block. This nests the post routes under the topic routes.
 
   get 'about' => 'welcome#about'
 
