7d97cad201928ce3bed5fb693516abbb4bfcf1e0
Aizaz Khaja <aizazkj@gmail.com>
2017-04-11 14:10:12 -0400

    asgn completed. added a formatting method for username with a callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?" #this is for setting a new user password
@@ -12,4 +14,14 @@ class User < ActiveRecord::Base
 
   has_secure_password
   # Adds methods to set and authenticate against a BCrypt password. This mechanism requires you to have a password_digest attribute.
+
+  def format_name
+    if name
+      name_arr = []
+      name.split.each do |name_part|
+        name_arr << name_part.capitalize
+      end
+      self.name = name_arr.join(' ')
+    end
+  end
 end
