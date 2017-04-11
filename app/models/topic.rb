5be15117fccc4411356e70a957a36b8fde46e6f8
Aizaz Khaja <aizazkj@gmail.com>
2017-04-10 22:34:53 -0400

    asgn23 complete. added topics validations

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   # when we delete topics, we want to make sure that associated posts are also deleted
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: {minimum: 15}, presence: true
 end
