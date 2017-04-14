0ef3f28e022f316b6ead81970029808a16ef88a8
Aizaz Khaja <aizazkj@gmail.com>
2017-04-14 18:42:22 -0400

    chk28 complete. added some roles to Users plus an enum for the different roles involved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :posts
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?" #this is for setting a new user password
@@ -13,4 +15,6 @@ class User < ActiveRecord::Base
 
   has_secure_password
   # Adds methods to set and authenticate against a BCrypt password. This mechanism requires you to have a password_digest attribute.
+
+  enum role: [:member, :admin]
 end
